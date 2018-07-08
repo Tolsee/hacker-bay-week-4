c9f0a913b24218fd8cea19688ee0d0a9d0099f18
Tolsee <tolsee3@gmail.com>
2018-07-08 19:34:22 +0545

    list fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   root: true, // make to not take in any user specified rules in parent folders
   parser: 'babel-eslint',
   extends: ['airbnb', 'prettier', 'prettier/flowtype', 'prettier/react'],
@@ -12,10 +12,10 @@ module.exports = {
     'react/prop-types': 0,
     'react/jsx-filename-extension': 0,
   },
-  "settings": {
-    "import/resolver": {
-      "node": {
-        "paths": ["src"]
+  'settings': {
+    'import/resolver': {
+      'node': {
+        'paths': ['src']
       }
     }
   }
